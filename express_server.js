0078dc4c561759fd5ccb76ca11de4587e0191f98
mrludovicc <nadal46@hotmail.com>
2023-02-21 14:23:05 -0500

    Adding redirecting to the URL and adding the url to the list

--- express_server.js
+++ express_server.js
@@ -4,9 +4,9 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-function generateRandomString() {
+const generateRandomString = () => {
   let random = Math.random().toString(36).slice(2);
-  console.log(random.slice(0,6))
+  return random.slice(0,6);
 }
 generateRandomString()
 const urlDatabase = {
@@ -15,6 +15,7 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
+  //console.log(res)
   res.send("Hello!");
 });
 
@@ -22,28 +23,34 @@ app.get('/urls', (req, res) => {
   const templateVars = { 
     urls: urlDatabase
   }
+  //console.log(templateVars.urls)
   res.render('urls_index', templateVars )
 })
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new")
+ // console.log(urlDatabase[req.params.id])
 })
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const random = generateRandomString()
+  urlDatabase[random] = req.body.longURL;
+  //console.log('verify long url:', req.body.longURL); // Log the POST request body to the console
+  res.redirect(`/urls/${random}`); 
+});
+
+app.get("/u/:id", (req, res) => {
+
+   const longURL = urlDatabase[req.params.id];
+   console.log('req: ', urlDatabase[req.params.id])
+  res.redirect(longURL);
 });
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
