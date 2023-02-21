1ecc83d095d2f6d6805558f52ed6942a3179348e
mrludovicc <nadal46@hotmail.com>
2023-02-20 21:13:51 -0500

    Adding new urls page

--- express_server.js
+++ express_server.js
@@ -3,7 +3,12 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {
+  let random = Math.random().toString(36).slice(2);
+  console.log(random.slice(0,6))
+}
+generateRandomString()
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,11 +25,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars )
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  console.log(urlDatabase[req.params.id])
   res.render("urls_show", templateVars);
-})
+});
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
 // });
