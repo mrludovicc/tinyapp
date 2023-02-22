7020243324b350905ebbae94929bc0683cbb9ed2
mrludovicc <nadal46@hotmail.com>
2023-02-21 20:24:59 -0500

    Adding the login and display the name

--- express_server.js
+++ express_server.js
@@ -8,9 +8,10 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
 const generateRandomString = () => {
   let random = Math.random().toString(36).slice(2);
-  return random.slice(0,6);
+  return random.slice(0, 6);
 }
 generateRandomString()
 const urlDatabase = {
@@ -24,28 +25,29 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { 
+  const templateVars = {
+    username: req.cookies["username"],
     urls: urlDatabase
   }
   //console.log(templateVars.urls)
-  res.render('urls_index', templateVars )
+  res.render('urls_index', templateVars)
 })
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new")
- // console.log(urlDatabase[req.params.id])
+  // console.log(urlDatabase[req.params.id])
 })
 app.post("/urls", (req, res) => {
   const random = generateRandomString()
   urlDatabase[random] = req.body.longURL;
   //console.log('verify long url:', req.body.longURL); // Log the POST request body to the console
-  res.redirect(`/urls/${random}`); 
+  res.redirect(`/urls/${random}`);
 });
 
 app.get("/u/:id", (req, res) => {
 
-   const longURL = urlDatabase[req.params.id];
-   //console.log('req: ', urlDatabase[req.params.id])
+  const longURL = urlDatabase[req.params.id];
+  //console.log('req: ', urlDatabase[req.params.id])
   res.redirect(longURL);
 });
 
@@ -56,7 +58,7 @@ app.post('/urls/:id/delete', (req, res) => {
 app.post('/urls/:id', (req, res) => {
   const shortURL = req.params.id;
   let newURL = req.body.updatedURL;
-  if (!newURL.includes('http')){
+  if (!newURL.includes('http')) {
     newURL = `http://${newURL}`
   }
   console.log(shortURL)
@@ -68,9 +70,14 @@ app.post('/login', (req, res) => {
   //console.log(value)
   res.cookie('username', value)
   res.redirect('/urls')
+
 })
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
